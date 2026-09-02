87d7c1f01b46c8ace4a07db0222aaff8a03db721
irvingyael52@outlook.com <irvingyael52@outlook.com>
2026-09-02 12:49:56 -0700

    Se agrega metodo para contar y decirte los nombres repetidos.

--- pruebaarraylist/nombresArrayList.java
+++ pruebaarraylist/nombresArrayList.java
@@ -13,7 +13,30 @@ public class nombresArrayList {
                 + "/pruebaarraylist/listado.txt");
         ArrayList<String> namesList = readNamesFromFile(filePath.toString());
         printNames(namesList);
+
+
+        ContaryRepetidos(namesList);
     }
+    public static void ContaryRepetidos(ArrayList<String> namesList){
+        ArrayList<String> uniqueNames = new ArrayList<String>();
+        ArrayList<Integer> Contadores = new ArrayList<Integer>();
+        for(String linea : namesList){
+            String primerNombre = linea.trim().split(" ")[0];
+            int indice = uniqueNames.indexOf(primerNombre);
+            if(indice != -1){
+                int cantidadActual = Contadores.get(indice);
+                Contadores.set(indice, cantidadActual + 1);
+            } else {
+                uniqueNames.add(primerNombre);
+                Contadores.add(1);
+            }
+        }
+        System.out.println("Nombres únicos y sus repeticiones:");
+        for(int i = 0; i < uniqueNames.size(); i++){
+            System.out.println(uniqueNames.get(i) + ": " + Contadores.get(i) + " Veces");
+        }
+    }
+
     public static void printNames(ArrayList<String> namesList){
         for(String name : namesList){
             System.out.println(name);
